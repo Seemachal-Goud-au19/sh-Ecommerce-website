3c41d0b30ec52901dcd4a16f3e345b71638e95ef
Seemachal <seemachal.goud@infobeans.com>
2023-12-31 12:49:02 +0530

    Retrying on api fetch fail

--- src/routes/FetchAPI.js
+++ src/routes/FetchAPI.js
@@ -1,35 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 
- const FetchAPI = () => {
-const [movieList, setMovieList] =  useState([])
-const [loading, setLoading] = useState(false)
+const FetchAPI = () => {
+  const [movieList, setMovieList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [intervalId, setIntervalId] = useState(null);
 
-    const fetchData = async ()=>{
-      setLoading(true)
+  const fetchData = async () => {
+    setLoading(true)
     try {
-          const response = await fetch('https://swapi.dev/api/films');
-          const data = await response.json();
-          setMovieList(data.results);
-          setLoading(false)
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-        
-    }
+      const response = await fetch('https://swapi.dev/api/film');
+      console.log(response)
+      if (!response.ok) {
+        throw new Error('Something went wrong Retrying...')
+      }
+      const data = await response.json();
+      setMovieList(data.results);
 
-  if(loading){
-    return <div>Loading...</div>
+    } catch (error) {
+      setError(error.message)
+    }
+    setLoading(false)
+
   }
 
+
+  let content = <p>Movies not found</p>;
+
+  if (loading) {
+    content = <p>Loading...</p>
+  }
+  if (error && movieList.length === 0) {
+    content = <p>{error}</p>
+  }
+  if (!loading && movieList.length > 0) {
+    content = <ul>
+      {movieList.map((movie, index) => <li key={index}>{movie.title}</li>)}
+    </ul>
+
+  }
+
+  const handleFetchClick = () => {
+    console.log("cleared")
+    clearInterval(intervalId);
+    setError(null)
+  };
+
+  useEffect(() => {
+    if (error) {
+      const id = setInterval(fetchData, 5000);
+      setIntervalId(id);
+    }
+
+  }, [error]);
   return (
     <div>
       <h5>Movie List</h5>
       <Button variant="info" onClick={fetchData}>Fetch Movies</Button>
-      <ul>
-      {movieList.map((movie)=><li>{movie.title}</li>)}
-      </ul>
-      </div>
+      {error && <Button variant="info" onClick={handleFetchClick}>Stop Fetching</Button>}
+      {content}
+    </div>
   )
 }
 
