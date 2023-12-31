654172194b33f0f367398f4b2341743a0aaac68b
Seemachal <seemachal.goud@infobeans.com>
2023-12-31 11:29:36 +0530

    loading while fetching movies

--- src/routes/FetchAPI.js
+++ src/routes/FetchAPI.js
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
 
  const FetchAPI = () => {
-const [movieList, setMovieList] = useState([])
+const [movieList, setMovieList] =  useState([])
+const [loading, setLoading] = useState(false)
+
     const fetchData = async ()=>{
+      setLoading(true)
     try {
           const response = await fetch('https://swapi.dev/api/films');
           const data = await response.json();
           setMovieList(data.results);
+          setLoading(false)
         } catch (error) {
           console.error('Error fetching data:', error);
         }
         
     }
 
-    useEffect(()=>{
-        fetchData();
-    },[])
+  if(loading){
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
       <h5>Movie List</h5>
+      <Button variant="info" onClick={fetchData}>Fetch Movies</Button>
       <ul>
       {movieList.map((movie)=><li>{movie.title}</li>)}
       </ul>
