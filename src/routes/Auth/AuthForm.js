bf5fff798c4a56b9587ca1632b08cdf7b0efc7ba
Seemachal <seemachal.goud@infobeans.com>
2024-01-23 15:06:49 +0530

    updated login

--- src/routes/Auth/AuthForm.js
+++ src/routes/Auth/AuthForm.js
@@ -18,8 +18,35 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     setIsLoading(true)
-    if (isLogin) {
 
+    if (isLogin) {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAA-F4NnCJ7QsljnVQfEBEz2Eq8aqrIZ9I', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        setIsLoading(false)
+        if (res.ok) {
+        return res.json().then((data)=>{
+          console.log("login data",data)
+        })
+        }
+        else {
+          return res.json().then((data) => {
+            let errorMessage = "Authantication failed"
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage)
+          })
+        }
+      })
     } else {
       fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAA-F4NnCJ7QsljnVQfEBEz2Eq8aqrIZ9I', {
         method: 'POST',
